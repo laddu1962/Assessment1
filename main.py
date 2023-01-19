a0c2be3c5ec6017439a5a1183e51627e3ed673b1
EDWIN <2105320@brunel.ac.uk>
2023-01-19 18:04:56 +0000

    safe cracker

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 # recuperar la criatura = reclaim the creature
 player_class = 0
 
-print("""WAKE UP! WAKE UP! WAKE UP!
+print("""
+WAKE UP! WAKE UP! WAKE UP!
          You've been asleep for long enough!
-         
          We have a lot of questions to ask!
          If you want to see your friend again, you're going to answer me.
-         
          How did you break into Area 51?
          
-         The screen fades to black...""")
-
-print("""You wake up in a dark room with three bodies on the floor;
+         The screen fades to black...
+         
+         You wake up in a dark room with three bodies on the floor;
          The bodies:
          1. soldier 
          2. agent 
          3. scientist
          You have to chose one so you can transform into them,
-         meaning you will become on of those humans, each which hold a different 
-         rank. 
+         meaning you will become on of those humans, each which hold a 
+         different rank. 
          
          Your mission is to infiltrate the humans base and reclaim our fellow,
          from their prison. You do this by becoming on of them. They won't see 
-         it coming!""")
+         it coming!
+         """)
+
 
 choice = int(input("Which human are you going to choose, type the number? "))
 
@@ -49,14 +50,13 @@ if player_class != 0:
     class_selection(player_class)
 
 
-print("""Now that's out of the way, make your way
-             to Area 51, reclaim our fellow. 
-             
-             You make your way to the human base,
-             in a box created by them, which they call a 'car'
-             
-             You reach the gate of Area 51, and the soldier at the 
-             gate ask you a riddle..""")
+print("""
+Now that's out of the way, make your way
+to Area 51, reclaim our fellow. 
+                         
+You reach the gate of Area 51, and the soldier at the 
+gate ask you a riddle..
+             """)
 
 # riddles with 3 attempts
 answer = "war"
@@ -70,7 +70,7 @@ if choice == 1:
         break
     usrAttempts -= 1
   else:
-    print("not attempts left")
+    print("he's waking up put him back under")
 
 answer = "newspaper"
 usrAttempts = 3
@@ -83,7 +83,7 @@ if choice == 2:
         break
     usrAttempts -= 1
   else:
-    print("not attempts left")
+    print("he's waking up put him back under")
 
 answer = "ice"
 usrAttempts = 3
@@ -96,15 +96,19 @@ if choice == 3:
         break
     usrAttempts -= 1
   else:
-    print("not attempts left")
+    print("he's waking up put him back under")
 
-
+print("""
+You enter area 51, but there is another barrier in front of you
+You have to solve 3 questions in order to enter the main building!
+         
+         """)
 
 # safe cracker
 import random
 
 safe_code = []
-# populate list
+
 while len(safe_code) < 3:
     code = random.randint(3,12)
     if code not in safe_code:
@@ -116,39 +120,24 @@ while codes_cracked < len(safe_code):
     for code in safe_code:
         print("enter number, that divide to %d" % code)
         try:
-            guess = input("type 1st number")
-            guess1 = input("type 2nd number")
+            guess = float(input("type 1st number"))
+            guess1 = float(input("type 2nd number"))
             result = round(guess / guess1)
             if result == code:
-                print("code cracked")
+                print("correct")
                 codes_cracked += 1
+
+
         except ValueError:
             print("That is not the number")
             break;
 
+        except ZeroDivisionError:
+            print("Can't divide by zero")
+
 else:
-    print("You have opened the safe")
+    print("You have opened the door")
 
 
-print(safe_code)
 
-# Weapons/ items
-inventory =["knife","dagger","tentacle sweeper" ]
-
-print("you have defeat the tentacle monster")
-print(inventory)
-
-choice = input("choose your weapon! ").lower()
-
-if choice not in inventory:
-    print("dead")
-    exit()
-
-else:print("you reach down and pull a % from your bag" % choice)
-
-if "tentacle sweeper" in choice:
-    print("you have picked the right weapon")
-
-elif "knife" or "dagger" in choice:
-    print("the weapon is weak, the monster killed you!")
 
